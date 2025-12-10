04f33be3ea9437b7b5f948f295c4e7a654269a30
lucaspires192 <felipe.silvamachado@yahoo.com>
2025-12-09 21:20:01 -0300

    subindo atualizacoes 09.12.2025 21:19

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,33 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = defaultConfig;
+// Configuração ESSENCIAL para TurboModules
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    ...config.resolver.extraNodeModules,
+    // Forçar resolução correta dos módulos do React Native
+    'react-native': require.resolve('react-native'),
+    'react': require.resolve('react'),
+  },
+  resolveRequest: (context, moduleName, platform) => {
+    // Resolver problemas com PlatformConstants
+    if (moduleName === 'PlatformConstants') {
+      return {
+        filePath: require.resolve('react-native/Libraries/Utilities/PlatformConstants'),
+        type: 'sourceFile',
+      };
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
+  sourceExts: [...config.resolver.sourceExts, 'cjs', 'mjs'],
+};
+
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+  // babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+module.exports = config;
